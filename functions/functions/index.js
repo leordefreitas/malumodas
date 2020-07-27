20c44477da3a41e7ad4ff0670a84fb9325222312
leordefreitas <leonardodefreitas1995@yahoo.com.br>
2020-07-27 18:16:00 -0300

    mudanças ao qual não me lembro o que foi alterado

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,5 +1,17 @@
 const functions = require('firebase-functions');
+const { db, admin } = require('./util/admin');
 const express = require('express');
 const app = express();
+
+app.post('/contato', criarContato = (req, res) => {
+  const novoContato = {
+    nome: req.body.nome,
+    whatsapp: req.body.whatsapp,
+    email: req.body.email,
+    criadoEm: new Date().toISOString()
+  };
+  
+
+});
 
 exports.api = functions.https.onRequest(app);
--- functions/functions/util/admin.js
+++ functions/functions/util/admin.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 
 admin.initializeApp({
-  credential: admin.credential.cert(require(''))
+  credential: admin.credential.cert(require('../../../../../CHAVES SITES/malumodas-43869-firebase-adminsdk-hfaso-1f4e841f87.json'))
 });
 
 const db = admin.firestore();
