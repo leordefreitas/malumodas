8aef2c46af006edf596d48ba168dbc64410546f1
Leonardo Rodrigues de Freitas <leonardodefreitas1995@yahoo.com.br>
2020-09-03 12:05:36 -0300

    adionando a imagen na secao chamativa e estilizando

--- src/components/secChamativa/SecChamativa.js
+++ src/components/secChamativa/SecChamativa.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import './SecChamativa.css'
+import './SecChamativa.css';
+import imagenChamativa from '../../assets/img/pexels-img/pexels-artem-beliaikin-2534965.jpg';
 
 function SecChamativa() {
   return (
     <div className="secao-chamativa">
-      
+      <div className="texto-secao-chamativa">
+        <p id="titulo-secao-chamativa">Malu Modas</p>
+        <p id="complemento-secao-chamativa">
+          Babozeiras e bobagens vai estudar meu filho, eu nao acreditei na minha mae
+        </p>
+      </div>
+      <img src={imagenChamativa} alt="imagen secao chamativa" />
     </div>
   )
 }
