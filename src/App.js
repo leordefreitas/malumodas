424a77802ad9f6eea72291c063160cd48fbbc661
Leonardo Rodrigues de Freitas <leonardodefreitas1995@yahoo.com.br>
2020-09-03 10:27:42 -0300

    comecando a trabalhar no header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 
+// componentes
+import Header from './components/header/Header';
+
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
