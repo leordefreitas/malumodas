8f9767140a941dd525fe305e5d2ff122d5b5f0ed
Leonardo Rodrigues de Freitas <leonardodefreitas1995@yahoo.com.br>
2020-12-22 18:28:21 -0300

    estrturando o componente

--- src/components/secContato/SecContato.js
+++ src/components/secContato/SecContato.js
@@ -3,8 +3,16 @@ import './SecContato.css';
 
 function SecContato() {
   return (
-    <div className="secao-entreEmContato">
-      
+    <div className="secao-contato">
+      <div id="input-secao-contato"></div>
+      <div id="informacao-secao-contato">
+        <div id="loja-informacao-secao-contato">
+          <p id="titulo-loja">Malu Modas</p>
+          <p id="endereco-loja">Endereço: Avenida 22 de novembro, 95 - Fonseca - Niteroi - RJ</p>
+          <p id="telefone-loja">Whatsapp: (48) 99665-1629</p>
+          <p id="email-loja">Email: leonardodefreitas1995@yahoo.com.br</p>
+        </div>
+      </div>
     </div>
   )
 }
